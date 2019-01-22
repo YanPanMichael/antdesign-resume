d1736976cdcb3878adcf0aeb85a52bb8c3b77d18
YanPanMichael <yanpancsu@icloud.com>
2019-01-23 00:03:37 +0800

    add antd spin loading and fetch-json

--- src/components/BackgroundContent.js
+++ src/components/BackgroundContent.js
@@ -1,39 +1,71 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import data from '../mockData/mockData.js';
+// http://api.douban.com/v2/movie/top250?start=25&count=25
+import mockdata from "../mockData/mockData.js";
+
+import fetchJsonp from 'fetch-jsonp';
+import { Spin, Alert } from "antd";
 
 class BackgroundContent extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      loadComplate: false
-    }
+      loadComplate: false,
+      dataList: [],
+    };
   }
-  
+
   componentWillMount() {
-    console.log("OKKK");
-    
-    fetch('https://www.baidu.com/home/msg')
-    .then(response => {
-      return response.json()
-    })
-    .then(data => console.log(data)
-    )
+    console.log("OKKK " + new Date());
+    this.loadDataFromServices();
+  }
+
+  loadDataFromServices() {
+    setTimeout(() => {
+      console.log("fff "+new Date());
+      this.setState({
+        loadComplate: true,
+        dataList: mockdata.subjects,
+      });
+    }, 2000);
+    // fetchJsonp("http://api.douban.com/v2/movie/top250?start=25&count=25")
+    //   .then(function(response) {
+    //     return response.json();
+    //   })
+    //   .then(function(json) {
+    //     console.log("parsed json", json);
+    //   })
+    //   .catch(function(ex) {
+    //     console.log("parsing failed", ex);
+    //   });
   }
 
   render() {
+    const { menu, page } = this.props.match.params;
+    const { dataList, loadComplate } = this.state;
     return (
-      <div>
-        BackgroundContent --- {this.props.match.params.menu + this.props.match.params.page}
-      </div>
+      <React.Fragment>
+        <div>
+          {loadComplate && `BackgroundContent --- ${menu} ${page}`}
+          {dataList && dataList.map(item => (
+            <div key={item.id}>{item.title}</div>
+          ))}
+          {!loadComplate && (
+            <Spin tip="Loading...">
+              <Alert
+                message="Please wait a while..."
+                description="Detail is comming..."
+                type="info"
+              />
+            </Spin>
+          )}
+        </div>
+      </React.Fragment>
     );
   }
 }
 
-BackgroundContent.propTypes = {
-
-};
+BackgroundContent.propTypes = {};
 
 export default BackgroundContent;
