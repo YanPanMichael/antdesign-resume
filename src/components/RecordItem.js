6a77a5e6b7bfc0412d5f714d4413f72be7dcac91
YanPanMichael <yanpancsu@icloud.com>
2019-01-24 01:49:49 +0800

    fix author array display problem

--- src/components/RecordItem.js
+++ src/components/RecordItem.js
@@ -10,7 +10,7 @@ class RecordItem extends Component {
       <div className={styles.itemStyle}>
         <img className={styles.itemImg} src={images.small.replace('img1','img3')} />
         <h4 className={styles.itemTitle}>{title}</h4>
-        <h4 className={styles.itemDirector}>{directors.join(', ')}</h4>
+        <h4 className={styles.itemDirector}>{directors.map(el=>el.name).join(', ')}</h4>
         <h4 className={styles.itemYear}>{year}</h4>
         <h4 className={styles.itemGenres}>{genres.join(', ')}</h4>
         <h4 className={styles.itemRating}>{rating.average}</h4>
